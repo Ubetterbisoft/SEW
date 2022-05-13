f67378f5aff551cb707516a55b3dc9a78bbb88af
Dejan Rajic <78872776+Ubetterbisoft@users.noreply.github.com>
2022-05-13 11:39:57 +0200

    Komplexe Muster fertig

--- 2 Klasse/Komplexere_Muster/src/Schwere_Grafik/Grafik.java
+++ 2 Klasse/Komplexere_Muster/src/Schwere_Grafik/Grafik.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 
 /**
  * es wird mit for die aktuelle positionierung der rechtecke berechnet
+ * @author draji
+ * @version 2022.05.13
  */
 public class Grafik extends JPanel {
     public Grafik(){
@@ -21,16 +23,22 @@ public class Grafik extends JPanel {
         int xVar = 0;
         int yVar = 0;
 
+        //count ist der Zähler um später zu Multiplizieren um den spaltenabstand zu erreichen
         int count = 0;
 
+        //es werden 10 zeile erstellt, i wird pro durchgang kleiner um es mit dem zweiten for zu vergleichen
         for(int i = 10;  i >0; i--){
             count++;
+            //solange die anzahl der Rechtecke kleiner ist als die anzahl der Zeilen (erstellt dsa gleichseitige dreieck)
             for(int j = 0;j < i;j++){
                 g.drawRect(mx-150,my-100,30,20);
                 mx = mx +30;
             }
+            //Pro neue zeile wird der y wert dazu genommen
             my = my +20;
+            // wird wieder auf 0 gesetzt
             mx = getWidth()/2;
+            //abstand wird berechnet
             mx = mx + count* 15;
 
 
