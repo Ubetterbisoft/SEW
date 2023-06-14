107b12309728562e0f1dc1e3e05c28b94efc83af
Dejan Rajic <78872776+Ubetterbisoft@users.noreply.github.com>
2023-06-14 16:36:46 +0200

    Update OrdnerAuflistung.java

--- 3 Klasse/EK_6c1.1_Auflistung_Eines_Ordners/src/OrdnerAuflistung.java
+++ 3 Klasse/EK_6c1.1_Auflistung_Eines_Ordners/src/OrdnerAuflistung.java
@@ -1,15 +1,23 @@
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+/**
+ * Dieses Programm erhält einen Pfad als parameter und iteriert durch den Ordner und gibt alle elemente samt unterordner an
+ * @author Dejan Rajic
+ * @version 14.06.2023
+ */
 public class OrdnerAuflistung {
-    public static String listFile(File file,String output){
+    public static String listFile(File file, int depth){
+        String output = "";
         File[] fileArray = file.listFiles();
         for(int i = 0; i < fileArray.length;i++){
             if(fileArray[i].isDirectory()){
-                output += "+" + fileArray[i].getName()+"\n";
-                output += listFile(fileArray[i],output);
+                output += "     ".repeat(depth)+" + "+ fileArray[i].getName()+"\n";
+                output += listFile(fileArray[i],depth+1);
             }
             if(fileArray[i].isFile()){
-                output += fileArray[i].getName()+"\n";
+                output += "     ".repeat(depth)+fileArray[i].getName()+" - "+ new SimpleDateFormat("dd.MM.yyyy").format(new Date(file.lastModified()))+" - "+ fileArray[i].length() +"Byte" +"\n";
             }
 
 
@@ -19,7 +27,7 @@ public class OrdnerAuflistung {
 
     public static void main(String[]args){
         File ordner = new File("C:\\Users\\draji\\OneDrive\\Desktop\\Github\\SEW\\3 Klasse\\EK_6c1.1_Auflistung_Eines_Ordners");
-        String output = "";
-        System.out.println(listFile(ordner,output));
+        int depth =0;
+        System.out.println(listFile(ordner,depth));
     }
 }
